3590c543fcbe1f8c4dfed449f0348cd13fbbff0e
Justin <info@justinlmartin.com>
2025-04-14 19:40:57 -0700

    swirl

---
+++ resources/js/index.js
@@ -0,0 +1,17 @@
+window.addEventListener('resize', function() {
+    setDimension();
+});
+
+function setDimension() {
+    const swirl = document.getElementById('swirl');
+    const innerHeight = window.innerHeight;
+    const innerWidth = window.innerWidth;
+    const radius = Math.round(Math.sqrt((innerHeight / 2) * (innerHeight / 2) + (innerWidth / 2) * (innerWidth / 2))) + 20;
+    const left = ((2 * radius) - innerWidth) / -2;
+    const top = ((2 * radius) - innerHeight) / -2;
+    swirl.style.setProperty('--radius', radius + 'px');
+    swirl.style.setProperty('--left', left + 'px');
+    swirl.style.setProperty('--top', top + 'px');
+}
+
+setDimension(); 
