c7cfa4909019f5701414453568e45b1500edf1e6
Justin <info@justinlmartin.com>
2025-04-14 19:51:12 -0700

    update

--- resources/js/index.js
+++ resources/js/index.js
@@ -6,7 +6,8 @@ function setDimension() {
     const swirl = document.getElementById('swirl');
     const innerHeight = window.innerHeight;
     const innerWidth = window.innerWidth;
-    const radius = Math.round(Math.sqrt((innerHeight / 2) * (innerHeight / 2) + (innerWidth / 2) * (innerWidth / 2))) + 20;
+    const radius = Math.max(window.innerHeight, window.innerWidth) / 2;
+    // const radius = Math.round(Math.sqrt((innerHeight / 2) * (innerHeight / 2) + (innerWidth / 2) * (innerWidth / 2))) + 20;
     const left = ((2 * radius) - innerWidth) / -2;
     const top = ((2 * radius) - innerHeight) / -2;
     swirl.style.setProperty('--radius', radius + 'px');
