45252b738c8c9dbd5a7a03387f86100e07b9257e
Justin <info@justinlmartin.com>
2025-04-16 22:04:24 -0700

    update

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,15 +3,15 @@ window.addEventListener('resize', function() {
 });
 
 function setDimension() {
-    const swirl = document.getElementById('swirl');
     const innerHeight = window.innerHeight;
     const innerWidth = window.innerWidth;
+    const swirlContainer = document.getElementById('swirl-container');
     const radius = Math.max(window.innerHeight, window.innerWidth) / 2;
     const left = ((2 * radius) - innerWidth) / -2;
     const top = ((2 * radius) - innerHeight) / -2;
-    swirl.style.setProperty('--radius', radius + 'px');
-    swirl.style.setProperty('--left', left + 'px');
-    swirl.style.setProperty('--top', top + 'px');
+    swirlContainer.style.setProperty('--radius', radius + 'px');
+    swirlContainer.style.setProperty('--left', left + 'px');
+    swirlContainer.style.setProperty('--top', top + 'px');
     const swirlOverlay = document.getElementById('swirl-overlay');
     swirlOverlay.style.setProperty('--radius', radius + 'px');
     swirlOverlay.style.setProperty('--left', left + 'px');
